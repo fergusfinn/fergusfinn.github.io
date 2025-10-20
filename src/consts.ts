d666bca8dad7889413010c96311e70d313b30d9a
Fergus <fergus.barratt00@gmail.com>
2025-10-20 16:20:46 +0100

    fix: add cover image, add header

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = ''
+export const SITE_TITLE = 'Fergus Finn'
 export const SITE_DESCRIPTION =
   'A blog about LLM inference'
 
